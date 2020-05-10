c3d9dbeec8963be3b8b2363e3fe7223b0aa8a941
Jacques Heunis <jaaksemail@gmail.com>
2020-05-10 08:13:27 +0100

    Fix the default game spec having spaces in the card names.

--- gamespec.go
+++ gamespec.go
@@ -114,58 +114,58 @@ func SerialiseSpecFromBytes(specBytes []byte) []byte {
 func DefaultSerializedGameSpec() []byte {
 	specStr := `---
 deck:
-- Ace Of Spades
-- 2 Of Spades
-- 3 Of Spades
-- 4 Of Spades
-- 5 Of Spades
-- 6 Of Spades
-- 7 Of Spades
-- 8 Of Spades
-- 9 Of Spades
-- 10 Of Spades
-- Jack Of Spades
-- Queen Of Spades
-- King Of Spades
-- Ace Of Clubs
-- 2 Of Clubs
-- 3 Of Clubs
-- 4 Of Clubs
-- 5 Of Clubs
-- 6 Of Clubs
-- 7 Of Clubs
-- 8 Of Clubs
-- 9 Of Clubs
-- 10 Of Clubs
-- Jack Of Clubs
-- Queen Of Clubs
-- King Of Clubs
-- Ace Of Diamonds
-- 2 Of Diamonds
-- 3 Of Diamonds
-- 4 Of Diamonds
-- 5 Of Diamonds
-- 6 Of Diamonds
-- 7 Of Diamonds
-- 8 Of Diamonds
-- 9 Of Diamonds
-- 10 Of Diamonds
-- Jack Of Diamonds
-- Queen Of Diamonds
-- King Of Diamonds
-- Ace Of Hearts
-- 2 Of Hearts
-- 3 Of Hearts
-- 4 Of Hearts
-- 5 Of Hearts
-- 6 Of Hearts
-- 7 Of Hearts
-- 8 Of Hearts
-- 9 Of Hearts
-- 10 Of Hearts
-- Jack Of Hearts
-- Queen Of Hearts
-- King Of Hearts
+- Ace-Of-Spades
+- 2-Of-Spades
+- 3-Of-Spades
+- 4-Of-Spades
+- 5-Of-Spades
+- 6-Of-Spades
+- 7-Of-Spades
+- 8-Of-Spades
+- 9-Of-Spades
+- 10-Of-Spades
+- Jack-Of-Spades
+- Queen-Of-Spades
+- King-Of-Spades
+- Ace-Of-Clubs
+- 2-Of-Clubs
+- 3-Of-Clubs
+- 4-Of-Clubs
+- 5-Of-Clubs
+- 6-Of-Clubs
+- 7-Of-Clubs
+- 8-Of-Clubs
+- 9-Of-Clubs
+- 10-Of-Clubs
+- Jack-Of-Clubs
+- Queen-Of-Clubs
+- King-Of-Clubs
+- Ace-Of-Diamonds
+- 2-Of-Diamonds
+- 3-Of-Diamonds
+- 4-Of-Diamonds
+- 5-Of-Diamonds
+- 6-Of-Diamonds
+- 7-Of-Diamonds
+- 8-Of-Diamonds
+- 9-Of-Diamonds
+- 10-Of-Diamonds
+- Jack-Of-Diamonds
+- Queen-Of-Diamonds
+- King-Of-Diamonds
+- Ace-Of-Hearts
+- 2-Of-Hearts
+- 3-Of-Hearts
+- 4-Of-Hearts
+- 5-Of-Hearts
+- 6-Of-Hearts
+- 7-Of-Hearts
+- 8-Of-Hearts
+- 9-Of-Hearts
+- 10-Of-Hearts
+- Jack-Of-Hearts
+- Queen-Of-Hearts
+- King-Of-Hearts
 `
 	return SerialiseSpecFromBytes([]byte(specStr))
 }
